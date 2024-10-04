805eef55a1fb356e868e0e52c0b1cc55795f26ca
Harshit Joshi <harshitj@stanford.edu>
2024-10-04 12:28:38 -0700

    update hotel search api

--- assignment/hotel_search.py
+++ assignment/hotel_search.py
@@ -3,17 +3,30 @@ from uuid import UUID, uuid4
 
 
 def hotel_search(
-    hotel_name: str,
-    location: str,
-    cost: str,
-    min_rating: str,
-    max_rating: str,
+    hotel_name: str | None,
+    location: str | None,
+    cost: str | None,
+    min_rating: str | None,
+    max_rating: str | None,
 ):
+    if min_rating is None or min_rating == "NA":
+        min_rating = 0
+    if max_rating is None or max_rating == "NA":
+        max_rating = 5
     min_rating = int(min_rating)
     max_rating = int(max_rating)
 
     min_rating_limit = 0
     max_rating_limit = 5
+
+    if hotel_name is None or hotel_name == "NA":
+        hotel_name = random.sample(["ShadySide Inn", "Grand Hotel", "Hilton Hotel"])
+
+    if location is None or location == "NA":
+        location = random.sample(["New York", "San Francisco", "Los Angeles"])
+
+    if cost is None or cost == "NA":
+        cost = random.sample(["cheap", "moderate", "expensive"])
 
     if min_rating < min_rating_limit or min_rating > max_rating_limit:
         return {
